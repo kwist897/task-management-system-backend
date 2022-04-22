704c1f5c4da97dd9282335b7648f57d9785d28a1
Kwist <isoloviov@rdlab.info>
2022-04-22 16:39:24 +0300

    created eureka server

---
+++ service-discovery/src/test/java/org/solowev/tms/discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.solowev.tms.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
