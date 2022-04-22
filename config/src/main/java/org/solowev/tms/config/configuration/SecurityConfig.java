21e08cbc66fa7cd051e9df7384b65d53dce1ad85
Kwist <isoloviov@rdlab.info>
2022-04-22 16:38:57 +0300

    created config server

---
+++ config/src/main/java/org/solowev/tms/config/configuration/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.solowev.tms.config.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //@formatter:off
+        http.csrf().disable();
+        http
+            .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .httpBasic();
+        //@formatter:on
+    }
+}
